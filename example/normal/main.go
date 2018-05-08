14ea0a18025ed5a23fb8d473fe69eb177ca7df60
pzrr@qq.com <pzrr@qq.com>
2018-05-08 14:37:31 +0800

    add for - select{}

--- example/normal/main.go
+++ example/normal/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	dotlog.StartLogService("./log.conf")
+	dotlog.StartLogService("log.conf")
 	log1 := dotlog.GetLogger("ClassicsLogger")
 	log1.Trace("example-normal trace main")
 	log1.Debug("example-normal debug main")
 	log1.Info("example-normal info main")
 	log1.Warn("example-normal warn main")
 	log1.Error(errors.New("example-normal error main"), "example-normal error main")
-	for true {
-
+	for{
+		select{}
 	}
 }
